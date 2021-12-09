a506024a5b1d6d3f5a31b661ac1f42b1360ad52b
Code Cadet <joaopedropacheco@enta.pt>
2021-12-09 18:51:08 -0100

    add character classes

---
+++ src/main/java/characters/Character.java
@@ -0,0 +1,15 @@
+package characters;
+
+import java.util.HashMap;
+
+public class Character {
+    protected HashMap<String, String > jokeList;
+
+    public HashMap<String, String> getJokeList() {
+        return jokeList;
+    }
+
+    public void setJokeList(HashMap<String, String> jokeList) {
+        this.jokeList = jokeList;
+    }
+}
